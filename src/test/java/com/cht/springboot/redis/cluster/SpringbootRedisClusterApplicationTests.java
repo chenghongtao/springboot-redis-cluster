d1a26cc4925338a28f51802c7449df3be5c80268
chenghongtao <18729553226@163.com>
2020-03-11 18:31:38 +0800

    project init

---
+++ src/test/java/com/cht/springboot/redis/cluster/SpringbootRedisClusterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cht.springboot.redis.cluster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRedisClusterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
