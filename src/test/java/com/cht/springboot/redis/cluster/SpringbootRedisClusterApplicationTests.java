e8b1973888e598e27a1a801e726c8f94b9fb19b8
chenghongtao <18729553226@163.com>
2020-03-11 18:40:30 +0800

    项目启动成功

--- src/test/java/com/cht/springboot/redis/cluster/SpringbootRedisClusterApplicationTests.java
+++ src/test/java/com/cht/springboot/redis/cluster/SpringbootRedisClusterApplicationTests.java
@@ -1,6 +1,6 @@
 package com.cht.springboot.redis.cluster;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
